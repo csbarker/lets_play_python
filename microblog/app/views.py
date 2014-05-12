806ed746e169a7f2de4b94ac4c1bec3c1fbf00fe
Callum Barker <callum.scott.barker@gmail.com>
2014-05-12 22:56:37 +0800

    step 3 - configs, webforms (wtforms),, flash messages

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,9 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
+#### INDEX ##############################
+#########################################
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = { 'nickname': 'Miguel' } # fake user
     posts = [ # fake array of posts
@@ -20,3 +23,17 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
+
+#### LOGIN ##############################
+#########################################
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
