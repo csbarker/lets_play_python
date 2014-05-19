3383b799e7ad20463887d219c9ff8359d191e399
Callum Barker <callum.scott.barker@gmail.com>
2014-05-19 21:58:17 +0800

    step 4 - databases
    learning how to query the database. Performed basic selects, inserts and
    deletes.

--- microblog/config.py
+++ microblog/config.py
@@ -1,5 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+## Settings
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+## Common
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
